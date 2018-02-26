30555730efffd6dd64a6ea68eea9fafb5035d026
BrianBend9 <BrianBend9@users.noreply.github.com>
2018-02-25 19:50:17 -0500

    Correct property names in loader config.

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,38 +1,41 @@
+/* eslint-disable comma-dangle, function-paren-newline, no-useless-escape */
+
 const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        'app': path.join(__dirname, 'assets', 'app', 'main.ts')
-    },
+  entry: {
+    app: path.join(__dirname, 'assets', 'app', 'main.ts')
+  },
 
-    resolve: {
-        extensions: ['.js', '.ts']
-    },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: 'html-loader'
-            },
-            {
-                test: /\.css$/,
-                use: 'raw-loader'
-            }
-        ]
-    },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
 
-    plugins: [
-        // To fix warning as per https://github.com/angular/angular/issues/20357
-        new webpack.ContextReplacementPlugin(
-            /\@angular(\\|\/)core(\\|\/)esm5/, path.join(__dirname, 'assets', 'app')
-        )
-    ],
+      {
+        test: /\.css$/,
+        loader: 'raw-loader'
+      }
+    ]
+  },
 
-    output: {
-        path: path.join(__dirname, 'public', 'js', 'app'),
-        filename: 'bundle.js',
-        publicPath: '/js/app/'
-    }
+  plugins: [
+    // To fix warning as per https://github.com/angular/angular/issues/20357
+    new webpack.ContextReplacementPlugin(
+      /\@angular(\\|\/)core(\\|\/)esm5/, path.join(__dirname, 'assets', 'app')
+    )
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public', 'js', 'app'),
+    filename: 'bundle.js',
+    publicPath: '/js/app/'
+  }
 };
