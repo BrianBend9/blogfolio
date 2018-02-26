987342ad80ab848636cbac00e37ef7a6ca3635b0
BrianBend9 <BrianBend9@users.noreply.github.com>
2018-02-25 20:11:55 -0500

    Modify dev config to conditionally add Browsersync dev server.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,50 +1,54 @@
-require('dotenv').config()
-const path = require('path');
+/* eslint-disable comma-dangle */
+
+require('dotenv').config();
+
 const commonConfig = require('./webpack.config.common');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
+const browserSyncServer = new BrowserSyncPlugin({
+  host: '0.0.0.0',
+  port: process.env.DEVSERVER_PORT,
+  proxy: `http://0.0.0.0:${process.env.PORT}`,
+  files: ['assets/app/*.html', 'assets/app/*.css', 'assets/app/*.js', 'assets/app/*.map']
+});
+
 // Create development config based on common config file (i.e. merging properties)
-module.exports = Object.assign(
-    {}, 
 
-    commonConfig,
+// Allow passing of custom command-line arguemnts to config using env param
+// (see https://github.com/webpack/webpack/issues/2254#issuecomment-318364254)
 
-    // Add devtool, and chunkFilename properties to config
-    {devtool: 'cheap-module-eval-source-map'},
+module.exports = env => Object.assign(
+  {},
 
-    {output: 
-        {
-            ...commonConfig.output, 
-            chunkFilename: "[id].chunk.js"
-        }
+  commonConfig,
+
+  // Add devtool, and chunkFilename properties to config
+  { devtool: 'cheap-module-eval-source-map' },
+
+  {
+    output: {
+      ...commonConfig.output,
+      chunkFilename: '[id].chunk.js',
+    }
+  },
+
+  // Add loaders to config
+  commonConfig.module.rules.push(
+    {
+      test: /\.ts$/,
+      loaders: [
+        'awesome-typescript-loader',
+        'angular2-template-loader',
+      ]
     },
 
-    // Add loaders to config
-    commonConfig.module.rules.push(
-        {
-            test: /\.ts$/,
-            loaders: [
-                'awesome-typescript-loader',
-                'angular2-template-loader',
-            ]
-        },
+    {
+      test: /\.(ts|js)$/,
+      loader: 'angular-router-loader',
+    }
+  ),
 
-        {
-            test: /\.(ts|js)$/,
-            loaders: [
-                'angular-router-loader'
-            ]
-        }
-    ),
-
-    // Add development server BrowerSync config
-    commonConfig.plugins.push(
-        new BrowserSyncPlugin({
-            host: '0.0.0.0',
-            port: process.env.DEVSERVER_PORT,
-            proxy: `http://0.0.0.0:${process.env.PORT}`,
-            files: ['assets/app/*.html', 'assets/app/*.css', 'assets/app/*.js', 'assets/app/*.map']
-        })
-    )
-
+  // Do not add BrowserSync dev server if only building assets (i.e. buildOnly flag is passed
+  // from command-line)
+  env.buildOnly ? null : commonConfig.plugins.push(browserSyncServer)
 );
